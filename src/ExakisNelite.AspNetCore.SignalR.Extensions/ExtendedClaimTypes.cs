e6c4f3c26a9dc796002e68ea284327017b2e7d72
Loïc BLAIN <loic.blain@exakis-nelite.com>
2022-02-09 15:43:26 +0100

    Test disable warning for Claim type value

--- src/ExakisNelite.AspNetCore.SignalR.Extensions/ExtendedClaimTypes.cs
+++ src/ExakisNelite.AspNetCore.SignalR.Extensions/ExtendedClaimTypes.cs
@@ -8,8 +8,9 @@
         /// <summary>
         /// Object identifier claim type.
         /// </summary>
+#pragma warning disable S5332
         public const string ObjectIdentifier = "http://schemas.microsoft.com/identity/claims/objectidentifier";
-
+#pragma warning restore S5332
         /// <summary>
         /// Subject claim type.
         /// </summary>
